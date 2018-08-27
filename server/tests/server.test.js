1215dd461d0d0f5bf59ccc40838948c9049ebbcd
Braedon Gough <braedongough@Braedons-MBP.localdomain>
2018-08-27 14:41:36 +0200

    add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -16,7 +16,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID,
-    text: 'Second'
+    text: 'Second',
+    completed: true,
+    compeltedAt: 12345666
 }]
 
 
@@ -146,4 +148,42 @@ describe('DELETE /todos/:id', () => {
             .expect(404)
             .end(done)
         })
+})
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        const hexId = todos[0]._id.toHexString()
+        const text = 'new text'
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: true, 
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text)
+                expect(res.body.todo.completed).toBe(true)
+                expect(res.body.todo.completedAt).toBeA('number')
+            })
+            .end(done)
+    })
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        const hexId = todos[1]._id.toHexString()
+        const text = 'new test text 2'
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+            completed: false, 
+            text
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text)
+            expect(res.body.todo.completed).toBe(false)
+            expect(res.body.todo.completedAt).toNotExist()
+        })
+        .end(done)
+    })
 })
