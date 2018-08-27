a8cafbe742b3e05690c9831250d755257f0c13ef
Braedon Gough <braedongough@Braedons-MBP.localdomain>
2018-08-27 11:53:06 +0200

    test the DELETE /todos.:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest')
 const expect = require('expect')
-const {ObjectID} = require('mongodb')
+const {
+    ObjectID
+} = require('mongodb')
 
 const {
     app
@@ -16,6 +18,8 @@ const todos = [{
     _id: new ObjectID,
     text: 'Second'
 }]
+
+
 
 beforeEach((done) => {
     Todo.remove({}).then(() => {
@@ -41,7 +45,9 @@ describe('POST /todos', () => {
                     return done(err)
                 }
 
-                Todo.find({text}).then((todos) => {
+                Todo.find({
+                    text
+                }).then((todos) => {
                     expect(todos.length).toBe(1)
                     expect(todos[0].text).toBe(text)
                     done()
@@ -70,7 +76,7 @@ describe('POST /todos', () => {
 describe('GET /todos', () => {
     it('should get all todos', (done) => {
         request(app)
-            .get ('/todos')
+            .get('/todos')
             .expect(200)
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2)
@@ -104,4 +110,40 @@ describe('GET /todos/:id', () => {
             .expect(404)
             .end(done)
     })
+})
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        const hexId = todos[1]._id.toHexString()
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(hexId)
+        }).end((err, res) => {
+            if (err) {
+                return done(err)
+            }
+            
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).toNotExist()
+                done()
+            }).catch((e) => done(e))
+        })
+    })
+
+        it('should return 404 if todo not found', (done) => {
+            const hexId = new ObjectID().toHexString()
+            request(app)
+                .delete(`/todos/${hexId}`)
+                .expect(404)
+                .end(done)
+        })
+
+        it('should return 404 if objectID is invalid', (done) => {
+            request(app)
+            .delete(`/todos/123`)
+            .expect(404)
+            .end(done)
+        })
 })
