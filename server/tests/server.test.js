8fbaa0d38818d895dfe0d917d05afe254801ea1b
Braedon Gough <braedongough@Braedons-MacBook-Pro.local>
2018-09-11 20:43:09 +0200

    add DELETE /users/me/token test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -298,29 +298,45 @@ describe('POST /users/login', () => {
     })
 
     it('should reject invalid login', (done) => {
-        //pass invalid password
-        //expect 400
-        //expect no x-auth token
-        //user.tokens array length = 0
         request(app)
-        .post('/users/login')
-        .send({
-            email: users[1].email,
-            password: 'this password is incorrect'
-        })
-        .expect(400)
-        .expect((res) => {
-            expect(res.headers['x-auth']).toNotExist()
-        })
-        .end((err, res) => {
-            if (err) {
-                return done(err)
-            }
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: 'this password is incorrect'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist()
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
 
-            User.findById(users[1]._id).then((user) => {
-                expect(user.tokens.length).toBe(0)
-                done()
-            }).catch((e) => done(e))
-        })
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0)
+                    done()
+                }).catch((e) => done(e))
+            })
+    })
+})
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on log out', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            //console.log(users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0)
+                    done()
+                }).catch((e) => done(e))
+            })
     })
 })
